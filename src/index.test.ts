ffcbe83ddf6188e12de4813216157b09b339b6c7
Lincoln <117424099+codelincoln@users.noreply.github.com>
2022-11-16 22:13:31 -0800

    feat: add support for single number strings

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,4 +2,14 @@ import { rollDice } from "./index"
 
 test("single number expression", () => {
 	expect(rollDice("3")).toBe(3);
+	expect(rollDice("14")).toBe(14);
+	expect(rollDice("9001")).toBe(9001);
+});
+
+test("test invalid single words", () => {
+	let words_to_test = [ "moist", "three", "donuts", "fifteen" ];
+
+	words_to_test.forEach((word) => {
+		expect(() => { rollDice(word) }).toThrow(word);
+	});
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
 console.log("Let's throw some bones!");
 
 export function rollDice(diceExpression: string) {
-	return 3;
+	let result = parseInt(diceExpression);
+
+	if (Number.isInteger(result)) {
+		return result;
+	} else {
+		throw InvalidSymbolError(diceExpression);
+	}
 }
+
+function InvalidSymbolError(symbol: string) {
+	return {
+		"name": "Invalid Symbol Error",
+		"message": `Invalid Symbol: ${symbol}`
+	}
+}
+
