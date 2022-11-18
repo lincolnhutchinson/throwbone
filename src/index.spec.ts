82f9f835a165552cc5310fc5239cbf6c6e985120
Lincoln <117424099+codelincoln@users.noreply.github.com>
2022-11-17 22:48:06 -0800

    Rename rollDice to evaluateRoll

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,14 +1,14 @@
 import { assert } from 'chai';
-import { rollDice } from './index.js';
+import { evaluateRoll } from './index.js';
 
 describe('throwbone', function() {
-	describe('rollDice', function() {
+	describe('evaluateRoll()', function() {
 		it('should return a number when given a single number in a string', function() {
-			assert.equal(rollDice("3"), 3);
+			assert.equal(evaluateRoll("3"), 3);
 		});
 
 		it('should throw an error if handed a word it doesn\'t recognize', function() {
-			assert.throw(() => rollDice('moist'), 'moist');
+			assert.throw(() => evaluateRoll('moist'), 'moist');
 		});
 	});
 });
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import random from 'random';
 import seedrandom from 'seedrandom';
 
-export function rollDice(diceExpression: string) {
-	let result = parseInt(diceExpression);
+export function evaluateRoll(rollExpression: string) {
+	let result = parseInt(rollExpression);
 
 	if (Number.isInteger(result)) {
 		return result;
 	} else {
-		throw InvalidSymbolError(diceExpression);
+		throw InvalidSymbolError(rollExpression);
 	}
 }
 
